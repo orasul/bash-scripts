649779288ede1c9d27517bc27f146fdf7a421770
Rasul <rosmanov@ggasoftware.com>
2012-08-07 18:31:40 +0400

    cut now supports -d and -f, f - should be comma separated numbers of columns.

--- cut.sh
+++ cut.sh
@@ -5,45 +5,60 @@ usage ()
   echo "Usage: `basename $0` [ -c N-M ] file"
 }
 
-while getopts :hc: arg
+while getopts :hc:d:f: arg
 do
   case $arg in
     h)  usage ; exit 0 ;;
-    c)  NUMBERS=$OPTARG ;;
+    c)  NUMBERS=$OPTARG ; TYPE="CHAR" ;;   # characters
+    d)  DELIMETR=$OPTARG ;;                # delimetr
+    f)  FIELDS=$OPTARG ; TYPE="FIELD" ;;   # fields numbers
   esac
 done
 
-# NUMBERS - should be parsed, as it can contain "-" and ","
+DELIMETR=${DELIMETR:=" "}
 
-##IFS_OLD=$IFS
-#IFS=","
-#count=0
-#
-#for i in $NUMBERS
-#do
-#  count=$(( count+1 ))
-#  eval N[$count]=$i
-#done
-#
-#IFS=$IFS_OLD
-#
-#echo ${N[@]}
-#for s in ${N[*]}
-#do
-#  CURRN=${N[s]}
-#  echo $CURRN
-#done
-
-while read line
-do
-  IFS_OLD=$IFS
-  IFS=","
-  for i in $NUMBERS
+characters ()
+{
+  local IFS=","
+  for i in $1
   do
     [[ ${i} =~ "-" ]] && echo -n ${line:${i%%-*}-1:${i##*-}} || echo -n ${line:${i}-1:1}
   done
-  IFS=$IFS_OLD
   echo
+}
+
+fields ()
+{
+  local DELIM=$1
+  local FIELDS=$2
+  local IFS=","
+#  length_fields=${#FIELDS[@]}
+#  echo $length_fields
+#  field_count=0
+  for field in $FIELDS
+  do
+#    field_count=$(( field_count+1 ))
+    local IFS=$DELIM
+    count=0
+    for col in $line
+    do
+      count=$(( count+1 ))
+      [[ $count -eq $field ]] && echo -n $col && echo -n "$DELIM"
+#      [[ $count -eq $field ]] && echo -n $col && [[ $field_count -ne $length_fields ]] && echo -n "$DELIM"
+    done
+  done
+  echo
+}
+
+while read line
+do
+  if [[ $TYPE = "CHAR" ]]
+  then
+    characters $NUMBERS
+  elif [[ $TYPE = "FIELD" ]]
+  then
+    fields $DELIMETR $FIELDS
+  fi
 done
 
 
