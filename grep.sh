0350aa1025cdc64209d6510105062e1928fee0e1
Rasul Osmanov <orasul@mail.ru>
2012-08-13 00:49:48 +0400

    Added -o function to grep.

--- grep.sh
+++ grep.sh
@@ -5,16 +5,34 @@ usage ()
   echo "Usage: `basename $0` regexp file"
 }
 
-if [ ${#} -le 1 ]
-then
-  usage
-  exit 1
-fi
-
-while read line
+while getopts :ho arg
 do
-  if [[  $line =~ $1 ]]
-  then
-    echo $line
-  fi
-done <$2
+  case $arg in
+    h)  usage ; exit 0 ;;
+    o)  USETYPE=ONLY ;;
+  esac
+done
+
+shift $(( $OPTIND -1 ))
+
+(( ${USETYPE:=FULL} ))
+
+if [[ $USETYPE = FULL ]]
+then
+  while read line
+  do
+    if [[  $line =~ $1 ]]
+    then
+      echo $line
+    fi
+  done < $2
+elif [[ $USETYPE = ONLY ]]
+then
+  while read line
+  do
+    if [[  $line =~ $1 ]]
+    then
+      echo $BASH_REMATCH
+    fi
+  done < $2
+fi
